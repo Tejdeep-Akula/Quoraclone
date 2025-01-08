08be3a2713bc82cb3a648619262bb0774e1781f2
Tejdeep-Akula <tejdeepakula775@gmail.com>
2025-01-08 21:14:01 +0530

    added assembly files

--- obj/Debug/net7.0/QuoraClone.AssemblyInfo.cs
+++ obj/Debug/net7.0/QuoraClone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuoraClone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f77ba60d0555b02f582833553c1d8777a862e394")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuoraClone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuoraClone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
