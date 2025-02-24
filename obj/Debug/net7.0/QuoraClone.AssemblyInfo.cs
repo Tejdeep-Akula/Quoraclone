3951b6274ff4bd968c6106faa9b78b43a34b8adb
Tejdeep-Akula <tejdeepakula775@gmail.com>
2025-02-25 00:28:47 +0530

    Generated by the MSBuild WriteCodeFragment class.

--- obj/Debug/net7.0/QuoraClone.AssemblyInfo.cs
+++ obj/Debug/net7.0/QuoraClone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuoraClone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cccbafb31e21700b1d23ecfeb678e3d4e1a5dc06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11279f3731d73edc6b1f469d7e654ff3be42a937")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuoraClone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuoraClone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
